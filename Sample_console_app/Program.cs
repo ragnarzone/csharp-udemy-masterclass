8369b04a08dfc984148c5b296d3d6f2867c255c4
ragnarzone <ragnarzone@gmail.com>
2023-10-14 19:31:29 +0300

    Add project files.

---
+++ Sample_console_app/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sample_console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringsManipulation stringsManipulation = new StringsManipulation();
+            stringsManipulation.manipulateString();
+        }
+    }
+}
---
+++ Sample_console_app/StringsManipulation.cs
@@ -0,0 +1,14 @@
+﻿namespace Sample_console_app
+{
+    internal class StringsManipulation
+    {
+        public void manipulateString()
+        {
+            string name;
+            Console.WriteLine("Please enter your name and press enter : ");
+            name = Console.ReadLine();
+            string myNameUpperCase = String.Format("Upper case : {0}", name.ToUpper());
+            Console.WriteLine(myNameUpperCase);
+        }
+    }
+}
